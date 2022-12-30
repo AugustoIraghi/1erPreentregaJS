d1d92721e6377ac8016f3122b0d7afd6af908146
Augusto Salmoiraghi <augusto.iraghi@gmail.com>
2022-12-29 23:46:02 -0300

    Primer guardado para GitHub

--- app.js
+++ app.js
@@ -43,34 +43,27 @@ let masCosas= 1
 let precio = 0 
 let saldoNuevo = 0
 
+
+alert("Bienvenido a la tienda DrunkDwarf de bebidas alcoholicas.")
+
 while (masCosas== 1) {
     let opcion = prompt("Elija que quiere comprar, seleccione con un numero lo que desee:\n1. Smirnoff $300\n2. Baileys $1000\n3. Gancia $500\n4. Fernet $700\n5. Campari $400")
     saldoActual= sumarAlCarrito(opcion, saldoActual)
-    alert("Su saldo actual es de " + saldoActual)
+    alert("El precio total actual en su carro de compras es de " + saldoActual)
 
 
     masCosas= Number(prompt("Quieres mas cosas? \n 1 si quiere mas \n 2 si no quiere mas"))
 }
 
-alert("El precio total de todos sus productos es de " + saldoActual)
+alert("El precio total de todos sus productos es de $" + saldoActual)
 
 let descuento = prompt("Introduzca un codigo de descuento si tiene disponible.\n (El codigo para el descuento es 4679 je)")
 
 if(descuento == "4679"){
-    alert("Se le ha realizado un 20% de descuento sobre su compra, el precio a pagar es de $"+ descuentoRealizado(saldoActual))
+    alert("Se le ha realizado un 20% de descuento sobre su compra, el precio final a pagar es de $"+ descuentoRealizado(saldoActual))
 }
 else{
     alert("El codigo de descuento que ha ingresado es invalido. Total a pagar: $"+ saldoActual)
 }
 
 alert("Gracias por comprar con nosotros!")
-
-
-
-/* 
-alert(prompt("Quiere comprar algo?"))
-
-switch(siOno){
-    case "si":
-
-} */
